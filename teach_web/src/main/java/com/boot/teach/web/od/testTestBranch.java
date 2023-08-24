df3bd370606e7f63e9b4fcb58c3175b1bd8ed0f4
王京 <1670373895@qq.com>
2023-08-24 22:55:26 +0800

    fix[testTestBranch]:修改测试分支测试类 1.修改commit

--- teach_web/src/main/java/com/boot/teach/web/od/testTestBranch.java
+++ teach_web/src/main/java/com/boot/teach/web/od/testTestBranch.java
@@ -3,6 +3,6 @@ package com.boot.teach.web.od;
 public class testTestBranch {
     public static void main(String[] args) {
         System.out.println("add one difference");
-        System.out.println("this is test branch");
+        System.out.println("this is new  test branch");
     }
 }
