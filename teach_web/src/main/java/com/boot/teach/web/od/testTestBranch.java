90567c9985d46059ebb464cbeaccca09a42ea58a
王京 <1670373895@qq.com>
2023-08-24 22:42:09 +0800

    fix[testTestBranch]:修改测试分支测试类 1.修改输出语句

--- teach_web/src/main/java/com/boot/teach/web/od/testTestBranch.java
+++ teach_web/src/main/java/com/boot/teach/web/od/testTestBranch.java
@@ -2,6 +2,7 @@ package com.boot.teach.web.od;
 
 public class testTestBranch {
     public static void main(String[] args) {
+        System.out.println("add one difference");
         System.out.println("this is test branch");
     }
 }
